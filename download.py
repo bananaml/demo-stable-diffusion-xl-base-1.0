34d4c36544f105418caf1c2b6cadd75d78c7af05
yachty66 <maxhager28@gmail.com>
2023-08-29 17:32:25 +0200

    fixed bug

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ MODEL = "stabilityai/stable-diffusion-xl-base-1.0"
 
 def download_model() -> tuple:
     """Download the model"""
-    model = DiffusionPipeline.from_pretrained(MODEL, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+    model = DiffusionPipeline.from_pretrained(MODEL, use_safetensors=True)
 
 
 if __name__ == "__main__":
